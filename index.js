3479b90d822d60438679a5379cba18dc31fd5914
wayungi <fwayungi@gmail.com>
2024-11-02 09:01:39 +0300

    ADD: buitl in middleware

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-
+require('dotenv').config()
+const express =  require('express')
 const EventEmitter = require('node:events');
-const myEmitter = new EventEmitter();
 const logEvents =  require('./utils/LogEvents')
+const app =  express();
+const PORT = process.env.PORT || 3001
+
+
+/* apply middleware with app.use */
+app.use(express.urlencoded({extended: true})) /* urlencoded is form data*/
+app.use(express.json()) /* handle json data*/
+app.use(express.static(path.join(__dirname, '/public'))) /* sets folder accesbile by the public */
+
+
+
+
+
+app.listen(PORT, () => { console.log(`Example app listening on port ${PORT}`) })
+
+//const myEmitter = new EventEmitter();
 
 // Log an evenyt
-myEmitter.on('logEvent', (msg) =>  logEvents(msg));
+//myEmitter.on('logEvent', (msg) =>  logEvents(msg));
 
 // myEmitter.emit('event', 1, 2, 3, 4, 5);
-myEmitter.emit('logEvent', 'Juma has deleted a file')
+//myEmitter.emit('logEvent', 'Juma has deleted a file')
+
+
